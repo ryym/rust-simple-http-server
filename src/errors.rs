a66460de8dfa43e58e22c29fd5fa85ae4c39bd78
ryym <ryym.64@gmail.com>
2017-11-26 14:46:00 +0900

    Create custom Error struct

---
+++ src/errors.rs
@@ -0,0 +1,51 @@
+use std::io;
+use std::fmt;
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct AppError {
+    message: String,
+    cause: Option<Box<Error>>,
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut ret = write!(f, "{}", &self.message);
+        if let Some(ref err) = self.cause {
+            ret = err.fmt(f);
+        }
+        ret
+    }
+}
+
+impl Error for AppError {
+    fn description(&self) -> &str {
+        self.cause.as_ref().map_or_else(|| self.message.as_str(), |e| e.description())
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        self.cause.as_ref().map(|e| &**e)
+    }
+}
+
+impl From<io::Error> for AppError {
+    fn from(err: io::Error) -> AppError {
+        AppError::by(Box::new(err))
+    }
+}
+
+impl AppError {
+    pub fn new<S: Into<String>>(message: S) -> AppError {
+        AppError {
+            message: message.into(),
+            cause: None,
+        }
+    }
+
+    pub fn by(cause: Box<Error>) -> AppError {
+        AppError {
+            message: String::new(),
+            cause: Some(cause),
+        }
+    }
+}
