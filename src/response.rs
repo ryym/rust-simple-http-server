68c8e54d5e1335450540f5f16e0d4bb47d6f5554
ryym <ryym.64@gmail.com>
2017-11-26 20:19:45 +0900

    Add Response headers

--- src/response.rs
+++ src/response.rs
@@ -1,43 +1,72 @@
 use std::io::prelude::*;
+use std::collections::HashMap;
 use super::{Status, AppResult};
+
+type Headers = HashMap<String, String>;
 
 pub struct Response<'a> {
     version: &'a str,
     status: Status,
     body: Option<Box<Read>>,
+    headers: Headers,
 }
 
 impl<'a> Response<'a> {
     pub fn new(status: Status) -> Response<'a> {
-        Response {
+        let mut res = Response {
             version: "HTTP/1.1",
             status,
             body: None,
-        }
+            headers: HashMap::new(),
+        };
+        Response::add_default_headers(&mut res);
+        res
+    }
+
+    fn add_default_headers(res: &mut Response) {
+        res.add_header("Server", "rust-simple-http-server");
+        res.add_header("Connection", "Close");
+        res.add_header("Content-Type", "text/plain");
     }
 
     pub fn set_body(&mut self, body: Box<Read>) {
         self.body = Some(body);
     }
 
-    // Maybe Response implements Read trait?
+    pub fn add_header<K: Into<String>, V: Into<String>>(&mut self, key: K, value: V) {
+        self.headers.insert(key.into(), value.into());
+    }
+
     pub fn into_string(&mut self) -> AppResult<String> {
-        let sline = self.make_statusline();
-        let res = match self.body.take() {
-            None => sline,
+        let body = match self.body.take() {
+            None => "".to_string(),
             Some(mut body) => {
                 let mut buf = String::new();
-                body.read_to_string(&mut buf)?;
-                sline + "\n" + &buf
+                let size = body.read_to_string(&mut buf)?;
+                self.add_header("Content-Length", size.to_string());
+                format!("\n{}", buf)
             }
         };
-        Ok(res)
+        let status = self.make_statusline();
+        let headers = self.headers_to_string();
+        Ok(format!("{}\n{}\n{}", status, headers, body))
     }
 
     fn make_statusline(&self) -> String {
-        format!("{} {} {}\n",
+        format!("{} {} {}",
                 self.version,
                 self.status.code(),
                 self.status.name())
     }
+
+    fn headers_to_string(&self) -> String {
+        self.headers.iter()
+            .map(|(k, v)| make_header(&k, &v))
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+}
+
+fn make_header(key: &str, value: &str) -> String {
+    format!("{}: {}", key, value)
 }
