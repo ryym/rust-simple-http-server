f5ef47159febc9708d6a46b9d972d042bd7420aa
ryym <ryym.64@gmail.com>
2017-11-26 16:23:37 +0900

    Reject access to parent directories

--- src/main.rs
+++ src/main.rs
@@ -31,17 +31,28 @@ fn handle_request(stream: &mut TcpStream, cwd: &PathBuf) -> AppResult<()> {
     let req = Request::from_stream(stream)?;
     let path = cwd.join(req.path().trim_matches('/'));
 
-    let mut res = if path.exists() && path.is_file() {
-        let file = File::open(&path)?;
-        let mut res = Response::new(Status::Ok);
-        res.set_body(Box::new(BufReader::new(file)));
-        res
-    } else {
-        Response::new(Status::NotFound)
+    let mut res = match path.canonicalize() {
+        Err(_) => Response::new(Status::NotFound),
+        Ok(path) => {
+            if !path.exists() || !is_allowed_path(&path, &cwd) {
+                Response::new(Status::NotFound)
+            } else if path.is_dir() {
+                Response::new(Status::NotFound)
+            } else {
+                let file = File::open(&path)?;
+                let mut res = Response::new(Status::Ok);
+                res.set_body(Box::new(BufReader::new(file)));
+                res
+            }
+        }
     };
 
     let res = res.into_string()?.into_bytes();
     BufWriter::new(stream).write(&res)?;
 
     Ok(())
+}
+
+fn is_allowed_path(path: &PathBuf, cwd: &PathBuf) -> bool {
+    path.starts_with(cwd)
 }
